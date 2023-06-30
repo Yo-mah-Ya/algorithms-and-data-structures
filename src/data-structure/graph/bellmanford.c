e19b8bfe7d8ee18dcd77bd4e48436d86ddc81931
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-06-29 20:13:06 -0700

    fix bellmanford logic

--- src/data-structure/graph/bellmanford.c
+++ src/data-structure/graph/bellmanford.c
@@ -19,7 +19,7 @@ typedef struct Graph {
  * @param dest Destination node
  * @returns Shortest distance from source to destination
  */
-int bellman_ford(Graph graph[], int V, int E, int src, int dest) {
+int *bellman_ford(Graph graph[], int V, int E, int src, int dest) {
     int dis[V];
     for (size_t i = 0; i < V; i++) {
         dis[i] = INT32_MAX;
@@ -31,29 +31,31 @@ int bellman_ford(Graph graph[], int V, int E, int src, int dest) {
     // vertex can have at-most |V| - 1 edges
     for (size_t i = 0; i < V - 1; i++) {
         for (size_t j = 0; j < E; j++) {
-            Graph jElement = graph[j];
-            if (dis[jElement.u] + jElement.w < dis[jElement.v]) {
-                dis[jElement.v] = dis[jElement.u] + jElement.w;
+            Graph g = graph[j];
+            if (dis[g.u] + g.w < dis[g.v]) {
+                dis[g.v] = dis[g.u] + g.w;
             }
         }
     }
 
     // check for negative-weight cycles.
     for (size_t i = 0; i < E; i++) {
-        const int u = graph[i].u;
-        const int v = graph[i].v;
-        const int w = graph[i].w;
-        if (dis[u] != INT32_MAX && dis[u] + w < dis[v]) {
+        Graph g = graph[i];
+        if (dis[g.u] != INT32_MAX && dis[g.u] + g.w < dis[g.v]) {
             printf(
                 "Graph contains negative weight cycle. Hence, shortest "
                 "distance not guaranteed.");
-            exit(1);
+            return NULL;
         }
     }
     for (size_t i = 0; i < V; i++) {
-        if (i == dest)
-            return dis[i];
+        if (i == dest) {
+            int *answer = (int *)malloc(sizeof(int));
+            *answer = dis[i];
+            return answer;
+        }
     }
+    return NULL;
 }
 
 Graph *get_Graph_struct(int graph_array[][3], int length) {
@@ -77,7 +79,9 @@ int main(int argc, char const *argv[]) {
 
         const int V = 5;
         const int destination = 3;
-        assert(bellman_ford(graph, V, E, 0, destination) == -2);
+        int *answer = bellman_ford(graph, V, E, 0, destination);
+        assert(*answer == -2);
+        free(answer);
     }
     {
         int graph_array[][3] = {
@@ -87,9 +91,11 @@ int main(int argc, char const *argv[]) {
         const int E = sizeof(graph_array) / sizeof(graph_array[0]);
         Graph *graph = get_Graph_struct(graph_array, E);
 
-        const V = 6;
-        const destination = 4;
-        assert(bellman_ford(graph, V, E, 0, destination) == 1);
+        const int V = 6;
+        const int destination = 4;
+        int *answer = bellman_ford(graph, V, E, 0, destination);
+        assert(*answer == 1);
+        free(answer);
     }
     {
         int graph_array[][3] = {
@@ -98,9 +104,11 @@ int main(int argc, char const *argv[]) {
         const int E = sizeof(graph_array) / sizeof(graph_array[0]);
         Graph *graph = get_Graph_struct(graph_array, E);
 
-        const V = 4;
-        const destination = 1;
-        assert(bellman_ford(graph, V, E, 0, destination) == 0);
+        const int V = 4;
+        const int destination = 1;
+        int *answer = bellman_ford(graph, V, E, 0, destination);
+        assert(*answer == 0);
+        free(answer);
     }
     return 0;
 }
